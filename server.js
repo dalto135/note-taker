be165a0018109dac6add6c927a3e3495c7b985bb
dalto135 <67397294+dalto135@users.noreply.github.com>
2021-04-10 15:38:22 -0400

    tweaked server functions

--- server.js
+++ server.js
@@ -3,6 +3,7 @@
 
 const express = require('express');
 const path = require('path');
+const db = require('./Develop/db/db.json');
 
 // EXPRESS CONFIGURATION
 // This sets up the basic properties for our express server
@@ -11,7 +12,7 @@ const path = require('path');
 const app = express();
 
 // Sets an initial port. We"ll use this later in our listener
-const PORT = process.env.PORT || 4200;
+const PORT = process.env.PORT || 3000;
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
@@ -23,6 +24,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 // These routes give our server a "map" of how to respond when users visit or request data from various URLs.
 
 require('./Develop/public/assets/js/index')(app);
+// require('./Develop/db/db.json')(app);
+
 
 // LISTENER
 // The below code effectively "starts" our server
@@ -32,13 +35,37 @@ app.listen(PORT, () => {
 });
 
 module.exports = (app) => {
+  //HTML routes
+  app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './Develop/public/notes.html'));
+  });
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './Develop/public/index.html'));
+  });
+  //API routes
   app.get("/api/notes", function(req, res) {
-    res.json(path.join(__dirname, "./Develop/public/index.js"));
+    res.json(db);
   });
   app.post("/api/notes", function(req, res) {
-    res.json(path.join(__dirname, "./Develop/public/index.js"));
+    db.push(req.body);
+    res.json(true);
   });
+  //Bonus
   app.delete(`/api/notes/:${id}`, function(req, res) {
-    res.json(path.join(__dirname, "./Develop/public/index.js"));
+    let array = [];
+    db.forEach(i, function() {
+      array.push(i);
+    });
+    array.forEach(j, function() {
+      if (j === id) {
+        array.pop(j);
+      }
+    });
+    db = '';
+    array.forEach(k, function() {
+      db += k;
+    });
+
+    res.json({ ok: true });
   });
 }
