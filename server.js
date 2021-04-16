5f7b72a4bed51b45b4f8bd7b28ebf48186a01be4
dalto135 <67397294+dalto135@users.noreply.github.com>
2021-04-16 02:31:36 -0400

    brought items in the develop folder to root

--- server.js
+++ server.js
@@ -14,12 +14,12 @@ const PORT = process.env.PORT || 3000;
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static(path.join(__dirname, './Develop/public')));
+app.use(express.static(path.join(__dirname, './public')));
 
 // ROUTER
 // The below points our server to a "route" file.
 // These routes give our server a "map" of how to respond when users visit or request data from various URLs.
-require('./Develop/public/assets/js/routes')(app);
+require('./public/assets/js/routes')(app);
 
 
 // LISTENER
